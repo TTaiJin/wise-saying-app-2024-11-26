3a64e9b81030b501b39b704c4df1e2e3f7a10d5d
TTaiJin <taeparis980110@gmail.com>
2024-11-26 12:38:17 +0900

    work

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,13 +1,14 @@
 package com.ll;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // lab1();
-        lab2();
+        // lab2();
+        // lab3();
+        lab4();
     }
 
     private static void lab1() {
@@ -34,4 +35,37 @@ public class Main {
         System.out.println("입력한 명령: " + cmd);
     }
 
+    private static void lab3() {
+        System.out.println("== 시작 ==");
+
+        System.out.println("안녕하세요.");
+        System.out.println("== 끝 ==");
+
+    }
+
+
+    private static void lab4() {
+        System.out.println("== 시작 ==");
+
+        // 출력이 모니터로 안되도록 설정
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕하세요.");
+        System.out.println("반갑습니다.");
+
+        // 다시 모니터로 출력되도록 설정
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+
+        System.out.println("== 끝 ==");
+
+        System.out.println("출력 : " + output.toString());
+
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
