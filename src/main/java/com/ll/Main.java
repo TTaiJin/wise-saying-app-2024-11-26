9037feece17d6d6e598122e7178d5ae883b37597
TTaiJin <taeparis980110@gmail.com>
2024-11-26 18:36:02 +0900

    work

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,23 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        lab1();
+        lab2();
+    }
+
+    private static void lab1() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("명령) ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령 : " + cmd);
+    }
+
+    private static void lab2() {
+
+    }
+}
